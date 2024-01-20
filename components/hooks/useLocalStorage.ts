83ea59faa2f92ec9350282505de7503fa2ca9170
Marcusgwj <Marcusgwj@hotmail.com>
2024-01-20 22:54:12 +0800

    Clean up

--- components/hooks/useLocalStorage.ts
+++ components/hooks/useLocalStorage.ts
@@ -14,28 +14,6 @@ const defaultData = {
     AllOfCompleted: new Set(),
     Completed: {},
     Pending: {},
-
-    // AllOfCompleted: new Set(),
-    // Completed: {
-    //     "AY 2021/2022": {
-    //         "Semester 1": ["Course 1000", "Course 2"],
-    //         "Semester 2": ["Course 3", "Course 4"],
-    //     },
-    //     "AY 2022/2023": {
-    //         "Semester 1": ["Course 5", "Course 6"],
-    //         "Semester 2": ["Course 7", "Course 8"],
-    //     },
-    // },
-    // Pending: {
-    //     "AY 2021/2022": {
-    //         "Semester 1": ["Pending Course 3", "Pending Course 2"],
-    //         "Semester 2": ["Pending Course 3", "Pending Course 4"],
-    //     },
-    //     "AY 2022/2023": {
-    //         "Semester 1": ["Pending Course 5", "Pending Course 6"],
-    //         "Semester 2": ["Pending Course 7", "Pending Course 8"],
-    //     },
-    // },
 };
 
 const useLocalStorage = create(
