261ed95282843c076739d3a007d19ceaeaad2890
Marcusgwj <Marcusgwj@hotmail.com>
2024-01-20 21:25:45 +0800

    Undo comment

--- app/(pages)/planner/page.tsx
+++ app/(pages)/planner/page.tsx
@@ -29,7 +29,19 @@ export default function PlannerPage() {
 
     return (
         <div className="h-screen">
-            (
+            {isEmptyObject(savedUserDetails) ? (
+                // please set up your tracker first, button display below
+                <div className="h-full flex flex-col w-full items-center justify-center">
+                    <div className="w-11/12 lg:w-10/12 xl:w-11/12 h-full flex flex-col justify-center items-center">
+                        <div className="text-xl lg:text-5xl text-center">
+                            Sorry! You need to set up your tracker before you
+                            are able to use the Grad Planner
+                            {/* #NOTE: Add a button here to redirect to set up */}
+                        </div>
+                        <Button className="mt-10">Set up now.</Button>
+                    </div>
+                </div>
+            ) : (
                 <div className="h-full flex flex-col w-full items-center">
                     <div className="w-11/12 lg:w-10/12 xl:w-11/12 h-full mt-32 lg:mt-28 tablet:mt-40 xl:mt-20 flex flex-col justify-center items-center">
                         <div className="mr-auto font-extrabold mb-5 tablet:text-xl xl:text-2xl">
@@ -38,7 +50,7 @@ export default function PlannerPage() {
                         <Planner />
                     </div>
                 </div>
-            )
+            )}
         </div>
     );
 }
