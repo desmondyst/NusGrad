912d464b7b64e580a293dea8d3ccebfcc398fbda
desmondyst <desmondyong06@gmail.com>
2024-01-20 14:15:05 +0800

    set up zustand for local storage

---
+++ components/hooks/useLocalStorage.ts
@@ -0,0 +1,57 @@
+"use client";
+
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+interface LocalStorage {
+    saved_data: {};
+}
+
+// https://docs.pmnd.rs/zustand/integrations/persisting-store-data
+
+const defaultData = {
+    userDetails: {},
+
+    // AllOfCompleted: new Set(),
+    // Completed: {
+    //     "AY 2023/2024": {
+    //         "Semester 1": ["Course 1", "Course 2"],
+    //         "Semester 2": ["Course 3", "Course 4"],
+    //     },
+    //     "AY 2024/2025": {
+    //         "Semester 1": ["Course 5", "Course 6"],
+    //         "Semester 2": ["Course 7", "Course 8"],
+    //     },
+    // },
+    // Pending: {
+    //     "AY 2023/2024": {
+    //         "Semester 1": ["Pending Course 1", "Pending Course 2"],
+    //         "Semester 2": ["Pending Course 3", "Pending Course 4"],
+    //     },
+    //     "AY 2024/2025": {
+    //         "Semester 1": ["Pending Course 5", "Pending Course 6"],
+    //         "Semester 2": ["Pending Course 7", "Pending Course 8"],
+    //     },
+    // },
+};
+
+const useLocalStorage = create(
+    persist<LocalStorage>(
+        (set, get) => ({
+            saved_data: defaultData,
+            addUserDetails: (newDetails) => {
+                const currentSavedData = get().saved_data;
+                const withAddedDetails = {
+                    ...currentSavedData,
+                    ["userDetails"]: newDetails,
+                };
+                set({
+                    saved_data: withAddedDetails,
+                });
+            },
+        }),
+        { name: "stored-data", storage: createJSONStorage(() => localStorage) }
+    )
+);
+
+export default useLocalStorage;
