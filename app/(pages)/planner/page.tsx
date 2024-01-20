51fe512ac84e40e8dce908578ad2a5f41a8a0eca
desmondyst <desmondyong06@gmail.com>
2024-01-20 23:20:23 +0800

    :construction:  WIP

--- app/(pages)/planner/page.tsx
+++ app/(pages)/planner/page.tsx
@@ -14,6 +14,8 @@ export default function PlannerPage() {
         return Object.keys(obj).length === 0;
     };
 
+    console.log(savedUserDetails);
+
     return (
         <div className="h-screen">
             {isEmptyObject(savedUserDetails) ? (
