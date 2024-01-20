842fa5bfb516c094a9808c525fe054cb000f3a88
Marcusgwj <Marcusgwj@hotmail.com>
2024-01-20 21:19:13 +0800

    Clean up

--- app/(pages)/planner/components/planner.tsx
+++ app/(pages)/planner/components/planner.tsx
@@ -1,46 +1,10 @@
 "use client";
 
-import { useCourses } from "@/components/hooks/useCourse";
 import { Button } from "@/components/ui/button";
 import CourseTable from "@/components/courseTable";
 import useLocalStorage from "@/components/hooks/useLocalStorage";
 
 const Planner = () => {
-    // To be updated with actual data
-    // const initialCourses = {
-    //     "AY 2023/2024": {
-    //         "Semester 1": ["Course 1", "Course 2"],
-    //         "Semester 2": ["Course 3", "Course 4"],
-    //     },
-    //     "AY 2024/2025": {
-    //         "Semester 1": ["Course 5", "Course 6"],
-    //         "Semester 2": ["Course 7", "Course 8"],
-    //     },
-    // };
-
-    const initialCourses = {
-        AllOfCompleted: new Set(),
-        Completed: {
-            "AY 2023/2024": {
-                "Semester 1": ["Course 1", "Course 2"],
-                "Semester 2": ["Course 3", "Course 4"],
-            },
-            "AY 2024/2025": {
-                "Semester 1": ["Course 5", "Course 6"],
-                "Semester 2": ["Course 7", "Course 8"],
-            },
-        },
-        Pending: {
-            "AY 2023/2024": {
-                "Semester 1": ["Pending Course 1", "Pending Course 2"],
-                "Semester 2": ["Pending Course 3", "Pending Course 4"],
-            },
-            "AY 2024/2025": {
-                "Semester 1": ["Pending Course 5", "Pending Course 6"],
-                "Semester 2": ["Pending Course 7", "Pending Course 8"],
-            },
-        },
-    };
 
     const localStorage = useLocalStorage();
     const completedCourses = localStorage.saved_data["Completed"];
